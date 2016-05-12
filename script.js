27e13327465ff856be3d39009273bb0b233e1783
Astro Acrylic <rad3845@gmail.com>
2016-05-11 21:22:30 -0700

    script-to-date

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ var today = new Date();
 var hourNow = today.getHours();
 var greeting;
 if (hourNow > 18) {
-	greeting = 'Good Evening';
+	greeting = 'Good Evening, Welcome';
 } else if (hourNow > 12) {
-	greeting = 'Good Afternoon';
+	greeting = 'Good Afternoon, Welcome';
 } else if (hourNow > 0) {
-	greeting = 'Good Morning';
+	greeting = 'Good Morning, Welcome';
 } else {
 	greeting = 'Welcome';
 }
