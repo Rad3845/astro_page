0513a1457fd4bc751800e662a4d43e913ec625b6
Astro Acrylic <rad3845@gmail.com>
2016-06-01 10:46:08 -0700

    update

--- Elec.js
+++ Elec.js
@@ -1,1 +1,13 @@
-confirm("Are you sure you want view this site owned by Astro Acrylic, your head may EXPLODE!!")
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+if (hourNow > 18) {
+	greeting = 'Astro Acrylic Presents';
+} else if (hourNow > 12) {
+	greeting = 'You Should be up';
+} else if (hourNow > 0) {
+	greeting = 'Wake Up!!';
+} else {
+	greeting = 'Welcome';
+}
+document.write(greeting);
