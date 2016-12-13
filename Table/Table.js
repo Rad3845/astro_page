e09c6868f0f933d0fe74dc3ae6e58f2dce7a111d
Richard Dorsey - Astro Acrylic <rad3845@gmail.com>
2016-12-13 10:09:43 -0700

    New Table

---
+++ Table/Table.js
@@ -0,0 +1,8 @@
+//define("bundles/videojs/layout/nls/circlePlayButton",["require","exports","module"],function(require,exports,module){module.exports={}});
+
+var msg = 'Acrylic Styles Newsletter Coming Soon!!';
+function updateMessage() {
+	var el = document.getElementById('two');
+	el.textContent = msg;
+}
+updateMessage();
---
+++ Table/js/Table.js
@@ -0,0 +1,10 @@
+var astro1 = 'HELLO ';
+
+var astro2 = 'WORLD ';
+
+var add = astro1 + astro2;
+document.write(add);
+
+var colors = ['black', 'white', 'gold', 'red', 'blue', 'pink'];
+
+document.write(colors[4])
