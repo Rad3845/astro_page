9832a60bf3b06bd60aae34d8e5919fcec75e2fba
Richard Dorsey - Astro Acrylic <rad3845@gmail.com>
2016-12-13 10:07:50 -0700

    Delete Table.js

--- Table/Table.js
+++
@@ -1,8 +0,0 @@
-//define("bundles/videojs/layout/nls/circlePlayButton",["require","exports","module"],function(require,exports,module){module.exports={}});
-
-var msg = 'Acrylic Styles Newsletter Coming Soon!!';
-function updateMessage() {
-	var el = document.getElementById('two');
-	el.textContent = msg;
-}
-updateMessage();
